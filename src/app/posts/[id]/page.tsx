392b21b4b731203f0b22281b5979274a90be6237
Mathieu <mathieu.dejob@gmail.com>
2025-01-14 22:48:20 +0100

    Update

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -15,6 +15,7 @@ export default async function PostPage({ params }: PostPageParams) {
     // TODO: Look into https://react.dev/reference/react/experimental_taintObjectReference
     // filter out sensitive data before passing to client.
     session.user = {
+      id: session.user.id,
       name: session.user.name,
       email: session.user.email,
       image: session.user.image,
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -55,6 +55,9 @@ declare module "next-auth" {
   interface Session {
     user: {
       id: string;
+      name: string;
+      email: string;
+      image: string;
     };
   }
 }
